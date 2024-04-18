f6225204cc941a2fe7cfbfbd4579010e1a67e645
Aleksandr Belousov <lksndrbelousov1983@mail.ru>
2024-04-18 11:35:52 +0700

    Unit tests and reports

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -3,15 +3,29 @@ const sorting = require("../../app");
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
     expect(
-      sorting.sortByName([
+      sorting.sortByName([        
         "Гарри Поттер",
         "Властелин Колец",
         "Волшебник изумрудного города",
       ])
-    ).toEqual([
+    ).toEqual([      
       "Властелин Колец",
       "Волшебник изумрудного города",
-      "Гарри Поттер",
-    ]);
+      "Гарри Поттер",          
+    ])
+    expect(
+      sorting.sortByName([
+        "Аладдин",
+        "Аладдин",
+        "Аладдин",
+      ])
+    ).toEqual([
+      "Аладдин",
+      "Аладдин",
+      "Аладдин",
+    ])                
   });
+  test("Without params throws exception", () => {
+    expect(() => sorting.sortByName()).toThrow(TypeError)
+  })
 });
