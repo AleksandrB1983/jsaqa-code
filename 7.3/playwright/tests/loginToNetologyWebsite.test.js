446c06f86e6596b2cae8d2ed28d99f8a7cb67982
Aleksandr Belousov <lksndrbelousov1983@mail.ru>
2024-04-22 22:28:51 +0700

    The second part with the issue

--- 7.3/playwright/tests/loginToNetologyWebsite.test.js
+++ 7.3/playwright/tests/loginToNetologyWebsite.test.js
@@ -1,19 +1,36 @@
 const { describe } = require("node:test");
-const { test, expect } = require("@playwright/test");
-const { login, password } = require("C:/Users/welcome/Desktop/jsaqa-code/7.3/playwright/user.js");
+const { test, expect, chromium } = require("@playwright/test");
+const { email, password } = require("C:/Users/welcome/Desktop/jsaqa-code/7.3/playwright/user.js");
 
-describe("Authorization on the netology website", async () => {
-    test("Successful authorization", async () => {
-      const browser = await chromium.launch({
-        headless: false,
-        slowMo: 5000,
-        devtools: true
-      });
-      const page = await browser.newPage();
-      await page.goto("https://netology.ru/?modal=sign_in");
-      enter (login, password).press("button"("Войти"));
-      await expect(page).toHaveURL(
-        "https://netology.ru/profile/8810739"
-      );
+describe("Authorization on the netology website", () => {
+  test("Successful authorization", async () => {
+    const browser = await chromium.launch({
+      headless: false,
+      slowMo: 5000,
+      devtools: true
+    });  
+    const page = await browser.newPage("https://netology.ru/?modal=sign_in");   
+    await page.goto("https://netology.ru/?modal=sign_in");
+    await page.getByLabel("Email").fill(email);
+    await page.getByLabel("Password").fill(password);
+    await page.getByRole("button", { name: "Sign in" }).click();
+    await page.waitForURL("https://netology.ru/");   
+    await expect(page.getByRole("button", { name: "https://netology.ru/profile/8810739" })).toBeVisible();  
+  })();  
+  
+  test("Unsuccessful authorization", async () => {
+    const browser = await chromium.launch({
+      headless: false,
+      slowMo: 5000,
+      devtools: true
     });
-});        
+    const page = await browser.newPage("https://netology.ru/?modal=sign_in");
+    const email1 = "qwerty123@main.ru";
+    const password1 = "qwerty890";
+    await page.goto("https://netology.ru/?modal=sign_in");
+    await page.getByLabel(input("Email")).fill(email1);
+    await page.getByLabel(input("Password")).fill(password1);
+    await page.getByRole("button", { name: "Sign in" }).click();    
+    await expect(page.getByRole("button", { name: "https://netology.ru/?modal=sign_in" })).toBeVisible();        
+  })();
+});
